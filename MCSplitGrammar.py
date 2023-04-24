1dc6ecd61afa6c240afbe106610378909444f0e2
kkwik <kevinkwik6@gmail.com>
2023-04-24 08:00:03 -0700

    feat: updated MCSplitGrammar to work in Amulet

--- MCSplitGrammar.py
+++ MCSplitGrammar.py
@@ -1,23 +1,33 @@
 from SplitGrammar import rule, split, reorient, void, fill, Dimension, Direction, Constraint, Scope, clearrules
-import utilityFunctions as uf
-from pymclevel import alphaMaterials, MCSchematic, MCLevel
 import SplitGrammar
 
+from amulet.api.block import Block
+from amulet.api.selection import SelectionGroup
 
 
 class MCScope(Scope):
     def __init__(self, level, box, **kwargs):
         super(MCScope, self).__init__(box, **kwargs)
-        self.level = level 
+        self.level = level
+
     def make_child(self, box, **kwargs):
         return MCScope(self.level, box, **kwargs)
         
     def set_material(self, material):
         for (x,y,z) in self.box.positions:
-            uf.setBlock(self.level, (material,0), x, y, z)
-            
-def start_symbol(box, level):
-    scope = MCScope(level, box, source="Root")
+            if material == 0:
+                material = Block.from_string_blockstate('minecraft:air')
+            self.level.set_version_block(x, y, z, 'minecraft:overworld', ("java", (1, 19, 3)), material) # set block
+
+
+def start_symbol(box: SelectionGroup, level: "BaseLevel"):
+    scope = MCScope(level=level, box=box, source="Root")
     return SplitGrammar.start_symbol(scope)
+
     
-SplitGrammar.register_material(-1, alphaMaterials.Cobblestone.ID)
+SplitGrammar.register_material(-1,Block.from_string_blockstate("minecraft:stone"))
+SplitGrammar.register_material(1, Block.from_string_blockstate("minecraft:cobblestone"))
+SplitGrammar.register_material(2, Block.from_string_blockstate("minecraft:dirt"))
+SplitGrammar.register_material(3, Block.from_string_blockstate("minecraft:granite"))
+SplitGrammar.register_material(4, Block.from_string_blockstate("minecraft:diorite"))
+SplitGrammar.register_material(5, Block.from_string_blockstate("minecraft:andesite"))
