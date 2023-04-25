ddd27422606968a4f87a75cd17333ebb3dd7212b
kkwik <kevinkwik6@gmail.com>
2023-04-24 20:23:14 -0700

    feat: updated MakeCastle, MakeTemple, and Tetrastyle to work in Amulet

--- MakeTemple.py
+++ MakeTemple.py
@@ -2,11 +2,12 @@ import time # for timing
 from math import sqrt, tan, sin, cos, pi, ceil, floor, acos, atan, asin, degrees, radians, log, atan2, acos, asin
 from random import *
 from numpy import *
-from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
-from mcplatform import *
 import json
 
-import utilityFunctions as uf
+from amulet.api.selection import SelectionGroup, SelectionBox
+from amulet.api.block import Block
+
+from GrammarBox import BoundingBox  
 
 from MCSplitGrammar import rule, split, reorient, void, fill, start_symbol, Dimension, Direction, Constraint, clearrules
 
@@ -21,7 +22,7 @@ COLUMNHEIGHT = 8
 
 clearrules(__file__)
 
-MARBLE = 155
+MARBLE = 6
 
 
 
@@ -194,10 +195,24 @@ def temples():
         temple()
         void()
         temple3()
-            
-def perform(level, box, options):
-    sc = start_symbol(box, level)
-    temples()
-    f = open("temple.json", "w")
-    json.dump(sc.to_json(True), f, indent=4)
-    f.close()
+
+def performOperation(
+    world: "BaseLevel",
+    dimension,
+    selection_group: SelectionGroup,
+    options: dict
+):
+    # Only operate on one selection box at a time. If there are multiple selection boxes in the selection group don't do anything
+    if len(selection_group.selection_boxes) == 1:
+        selection_box: SelectionBox = selection_group.selection_boxes[0]
+        bounding_box: BoundingBox = BoundingBox(selection_box.min, selection_box.shape) # Convert amulet Selection box into GrammarBox BoundingBox
+        sc = start_symbol(bounding_box, world)
+        temples()
+        f = open("temple.json", "w")
+        json.dump(sc.to_json(True), f, indent=4)
+        f.close()
+
+export = {
+    "name": "Make Temple",  # the name of the plugin
+    "operation": performOperation,  # the actual function to call when running the plugin
+}
--- Tetrastyle.py
+++ Tetrastyle.py
@@ -2,11 +2,12 @@ import time # for timing
 from math import sqrt, tan, sin, cos, pi, ceil, floor, acos, atan, asin, degrees, radians, log, atan2, acos, asin
 from random import *
 from numpy import *
-from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
-from mcplatform import *
 import json
 
-import utilityFunctions as uf
+from amulet.api.selection import SelectionGroup, SelectionBox
+from amulet.api.block import Block
+
+from GrammarBox import BoundingBox  
 
 from MCSplitGrammar import rule, split, reorient, void, fill, start_symbol, Dimension, Direction, Constraint, clearrules
 
@@ -21,7 +22,7 @@ COLUMNHEIGHT = 8
 
 clearrules(__file__)
 
-MARBLE = 155
+MARBLE = 6
 
 @rule 
 def level5roof():
@@ -94,7 +95,21 @@ def temple_walls():
 def temple():
     with split(Dimension.Y, [1, COLUMNHEIGHT, 5]):
         fill(MARBLE),temple_walls(),temple_roof()
-    
-def perform(level, box, options):
-    sc = start_symbol(box, level)
-    temple()
+
+def performOperation(
+    world: "BaseLevel",
+    dimension,
+    selection_group: SelectionGroup,
+    options: dict
+):
+    # Only operate on one selection box at a time. If there are multiple selection boxes in the selection group don't do anything
+    if len(selection_group.selection_boxes) == 1:
+        selection_box: SelectionBox = selection_group.selection_boxes[0]
+        bounding_box: BoundingBox = BoundingBox(selection_box.min, selection_box.shape) # Convert amulet Selection box into GrammarBox BoundingBox
+        sc = start_symbol(bounding_box, world)
+        temple()
+
+export = {
+    "name": "Make Tetrastyle",  # the name of the plugin
+    "operation": performOperation,  # the actual function to call when running the plugin
+}
